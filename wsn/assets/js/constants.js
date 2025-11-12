1c20ed903d790f2bbfa8f84e307da3f6f8f5621e
gabin-cxmp <gabin.touitou@comexposium.com>
2025-11-12 15:09:18 +0100

    Add files via upload

--- wsn/assets/js/constants.js
+++ wsn/assets/js/constants.js
@@ -14,8 +14,8 @@ export const dom = {
   };
 
 export const infoEvent = {
-    name: "WHO'S NEXT 2025",
-    abreviation: 'WSN-25'
+    name: "WHO'S NEXT JAN 2026",
+    abreviation: 'WSN-JAN-26'
   }
 
 export const SOCIALS = [
@@ -40,11 +40,11 @@ export const SOCIALS = [
       socials: ['instagram', 'facebook', 'x'], 
       dimensions: [1080, 1080], 
       logoCenter: [540, 1033], 
-      standLetterPosition: [1080-53, 1043], 
-      maxLogoSize: [200, 70], 
-      font: '32px NueueHaasDisplayMedium' 
+      standLetterPosition: [1080-28, 1043], 
+      maxLogoSize: [200, 64], 
+      font: '32px Acumin Condensed Bold' 
     },
-    { 
+   /* { 
       nameEN: 'Square format',
       nameFR:'Format carré', 
       dataId:'customized-square-format-curve',
@@ -56,23 +56,23 @@ export const SOCIALS = [
       logoCenter: [540, 1033], 
       standLetterPosition: [1080-53, 1043], 
       maxLogoSize: [200, 70], 
-      font: '32px NueueHaasDisplayMedium' 
-    },
+      font: '32px Acumin Condensed Bold' 
+    }, */
     { 
       nameEN: 'Story format', 
       nameFR: 'Format story', 
-      dataId:'customized-story-format-whos-next',
+      dataId:'WSN-JANV-26_custom-story',
       templateEN: 'assets/templates/1080x1920.png', 
       templateFR: 'assets/templates/1080x1920.png', 
       description: '(1080x1920)', 
       socials: ['instagram', 'facebook', 'tiktok'], 
       dimensions: [1080, 1920], 
       logoCenter: [540, 1873], 
-      standLetterPosition: [1080-53, 1883], 
-      maxLogoSize: [200, 70], 
-      font: '32px NueueHaasDisplayMedium' 
+      standLetterPosition: [1080-28, 1883], 
+      maxLogoSize: [200, 64], 
+      font: '32px Acumin Condensed Bold' 
     },
-    { 
+    /* { 
       nameEN: 'Story format', 
       nameFR: 'Format story', 
       dataId:'customized-story-format-curve',
@@ -84,23 +84,23 @@ export const SOCIALS = [
       logoCenter: [540, 1873], 
       standLetterPosition: [1080-53, 1883], 
       maxLogoSize: [200, 70], 
-      font: '32px NueueHaasDisplayMedium' 
-    },
+      font: '32px Acumin Condensed Bold' 
+    }, */
     { 
       nameEN: 'Portrait format', 
       nameFR: 'Format portrait', 
-      dataId:'customized-portrait-format-whos-next',
+      dataId:'WSN-JANV-26_custom-portrait',
       templateEN: 'assets/templates/1080x1350.png', 
       templateFR: 'assets/templates/1080x1350.png', 
       description: '(1080x1350)', 
       socials: ['facebook', 'instagram'], 
       dimensions: [1080, 1350], 
       logoCenter: [540, 1303], 
-      standLetterPosition: [1080-53, 1313], 
-      maxLogoSize: [200, 70], 
-      font: '32px NueueHaasDisplayMedium' 
+      standLetterPosition: [1080-28, 1313], 
+      maxLogoSize: [200, 64], 
+      font: '32px Acumin Condensed Bold' 
     },
-    { 
+    /* { 
       nameEN: 'Portrait format', 
       nameFR: 'Format portrait', 
       dataId:'customized-portrait-format-curve',
@@ -112,23 +112,23 @@ export const SOCIALS = [
       logoCenter: [540, 1303], 
       standLetterPosition: [1080-53, 1313], 
       maxLogoSize: [200, 70], 
-      font: '32px NueueHaasDisplayMedium' 
-    },
+      font: '32px Acumin Condensed Bold' 
+    }, */
     { 
       nameEN: 'LinkedIn Banner', 
       nameFR: 'Bannière Linkedin',
-      dataId:'customized-linkedin-banner-whos-next', 
+      dataId:'WSN-JANV-26_custom-linkedin', 
       templateEN: 'assets/templates/1584x396.png', 
       templateFR: 'assets/templates/1584x396.png', 
       description: '(1584x396)', 
       socials: ['linkedin'], 
       dimensions: [1584, 396], 
-      logoCenter: [792, 351], 
-      standLetterPosition: [1584-74, 362], 
-      maxLogoSize: [200, 70], 
-      font: '32px NueueHaasDisplayMedium'
+      logoCenter: [792, 361], 
+      standLetterPosition: [1584-28, 371], 
+      maxLogoSize: [200, 56], 
+      font: '32px Acumin Condensed Bold'
      },
-     { 
+     /* { 
       nameEN: 'LinkedIn Banner', 
       nameFR: 'Bannière Linkedin',
       dataId:'customized-linkedin-banner-curve', 
@@ -140,23 +140,23 @@ export const SOCIALS = [
       logoCenter: [792, 351], 
       standLetterPosition: [1584-74, 362], 
       maxLogoSize: [200, 70], 
-      font: '32px NueueHaasDisplayMedium'
-     },
+      font: '32px Acumin Condensed Bold'
+     }, */
      { 
       nameEN: 'Landscape format', 
       nameFR: 'Format paysage', 
-      dataId:'customized-landscape-format-whos-next',
+      dataId:'WSN-JANV-26_custom-landscape',
       templateEN: 'assets/templates/1200x628.png', 
       templateFR: 'assets/templates/1200x628.png', 
       description: '(1200x628)', 
       socials: ['facebook', 'instagram', 'x'], 
       dimensions: [1200, 628], 
-      logoCenter: [600, 583], 
-      standLetterPosition: [1200-58, 594], 
-      maxLogoSize: [200, 70], 
-      font: '32px NueueHaasDisplayMedium'
+      logoCenter: [600, 593], 
+      standLetterPosition: [1200-58, 603], 
+      maxLogoSize: [200, 56], 
+      font: '32px Acumin Condensed Bold'
      },
-     { 
+    /* { 
       nameEN: 'Landscape format', 
       nameFR: 'Format paysage', 
       dataId:'customized-landscape-format-curve',
@@ -168,23 +168,23 @@ export const SOCIALS = [
       logoCenter: [600, 583], 
       standLetterPosition: [1200-58, 594], 
       maxLogoSize: [200, 70], 
-      font: '32px NueueHaasDisplayMedium'
-     },
+      font: '32px Acumin Condensed Bold'
+     }, */
      { 
       nameEN: 'Email signature', 
       nameFR: 'Signature Email', 
-      dataId:'customized-email-signature-whos-next',
+      dataId:'WSN-JANV-26_custom-email',
       templateEN: 'assets/templates/600x140.png', 
       templateFR: 'assets/templates/600x140.png', 
       description: '(600x140)', 
       socials: ['mail'], 
       dimensions: [600, 140], 
-      logoCenter: [300, 127], 
-      standLetterPosition: [600-29, 131], 
-      maxLogoSize: [70, 18], 
-      font: '12px NueueHaasDisplayMedium'
+      logoCenter: [300, 128], 
+      standLetterPosition: [600-8, 131], 
+      maxLogoSize: [70, 16], 
+      font: '10px Acumin Condensed Bold'
      },
-     { 
+     /* { 
       nameEN: 'Email signature', 
       nameFR: 'Signature Email', 
       dataId:'customized-email-signature-curve',
@@ -196,23 +196,23 @@ export const SOCIALS = [
       logoCenter: [300, 127], 
       standLetterPosition: [600-29, 131], 
       maxLogoSize: [70, 18], 
-      font: '12px NueueHaasDisplayMedium'
-     },
+      font: '12px Acumin Condensed Bold'
+     }, */
      { 
       nameEN: 'Small banner', 
       nameFR: 'Petite bannière',
-      dataId:'customized-small-banner-whos-next', 
+      dataId:'WSN-JANV-26_custom-banner', 
       templateEN: 'assets/templates/300x250.png', 
       templateFR: 'assets/templates/300x250.png', 
       description: '(300x250)', 
       socials: ['google-ads', 'meta'], 
       dimensions: [300, 250], 
-      logoCenter: [150, 237], 
-      standLetterPosition: [300-15, 241], 
-      maxLogoSize: [34, 18], 
-      font: '12px NueueHaasDisplayMedium'
-     },
-     { 
+      logoCenter: [150, 238], 
+      standLetterPosition: [300-9, 241], 
+      maxLogoSize: [34, 16], 
+      font: '10px Acumin Condensed Bold'
+     }
+    /* { 
       nameEN: 'Small banner', 
       nameFR: 'Petite bannière',
       dataId:'customized-small-banner-curve', 
@@ -224,8 +224,8 @@ export const SOCIALS = [
       logoCenter: [150, 237], 
       standLetterPosition: [300-15, 241], 
       maxLogoSize: [34, 18], 
-      font: '12px NueueHaasDisplayMedium'
-     }
+      font: '12px Acumin Condensed Bold'
+     } */
   ];
   
 
