9104630b30c992fda1f0eb72311984b0f1164840
gabin-cxmp <gabin.touitou@comexposium.com>
2025-11-13 11:55:40 +0100

    Add files via upload

--- shared-assets/js/constants-loader.js
+++ shared-assets/js/constants-loader.js
@@ -10,6 +10,9 @@ if (window.location.href.toLowerCase().includes('bijorhca')) {
     constants = await import('../../premiere-classe/assets/js/constants.js');
 } else if (window.location.href.toLowerCase().includes('salon-international-de-la-lingerie')) {
     constants = await import('../../salon-international-de-la-lingerie/assets/js/constants.js');
+} else if (window.location.href.toLowerCase().includes('shoppe-object')) {
+    constants = await import('../../shoppe-object/assets/js/constants.js');
 }
+
 
 export const { dom, infoEvent, FORMATS_CUSTOMIZED_BANNERS, LOGOS } = constants;
